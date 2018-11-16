731326e940633fa4b70354f23960c51606814d15
TheSleepyEngineer <mahmoud.odb@gmail.com>
2018-11-15 22:41:00 -0500

    problem5: smallest multiple resolved

--- 5-Smallest_Multiple.py
+++ 5-Smallest_Multiple.py
@@ -1,1 +1,12 @@
-print("smallest multiple")
+import fractions
+
+def lcm(a, b):
+    gc = fractions.gcd(a,b)
+    return a*b/gc
+
+result = 1
+for i in range (1,21):
+    result = lcm(result, i)
+
+print(result)
+
