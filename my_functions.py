af2bf8b764c74b255f225f1ecd473c53ec435fee
mahmoud <mahmoud.odb@gmail.com>
2018-12-01 01:17:51 -0500

    problem 12: highly divisible triangular - in progress

--- my_functions.py
+++ my_functions.py
@@ -1,5 +1,6 @@
 import math
 
+#prime factors:
 def factors(n):
     fact=[]
     r = math.sqrt(n)
@@ -11,6 +12,18 @@ def factors(n):
                 fact.append(i)
                 n=n/i
     return fact
+
+#all factors:
+
+def all_factors(n):
+    fact=[]
+    r = n/2
+    for i in range (1,int(r)+1):
+        if n%i==0:
+            fact.append(i)
+    fact.append(n)
+    return fact
+
 
 def is_prime(a):
     for i in range(2,int(math.sqrt(a))+1):
