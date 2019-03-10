8cc10fb74ec4293a85dd567accf0c9fc28fa8f19
mahmoud <mahmoud.odb@gmail.com>
2019-03-10 08:23:02 -0400

    in progress

--- 17-Number_letter_counts.py
+++ 17-Number_letter_counts.py
@@ -1,37 +1,40 @@
 numbers_in_characters= {0:"", 1: "one", 2: "two", 3: "three", 4: "four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine" , 10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:" nineteen" , 20:"twenty", 30:"thirty", 40:"forty", 50:"fifty", 60:"sixty", 70:"seventy", 80:"eighty", 90:"ninety",100:"hundred", 1000:"thousand"}
 
 def convert_to_letters(x):
-    in_letters=0
-    i=1
-    s=[]
-    for i in range(0, len(x)):
-        s.append(x[i])
+    length_in_letters=0
+
+    # in case x == 1000
+    if x == "1000":
+        length_in_letters = len(numbers_in_characters[1]) + len("thousand")
+        return length_in_letters
 
 
-    if x[len(x) - 2] == '1' and int(x) > 9 :
-        in_letters += len(numbers_in_characters[int(s.pop()) + 10 ])
-        s.pop()
+    i = len(x)
 
-    else:
-        in_letters += len(numbers_in_characters[int(s.pop())])
+    if i==3:
+        string = numbers_in_characters[int(x[0])] + "hundred"
+        length_in_letters+= len(string)
+        i=-1
+        x= x[1] + x[2]
+        # in case 300 don't need the "and"
+        if int(x) != 0:
+            length_in_letters+= len("and")
 
-        if s!=[]:
-            in_letters +=len(numbers_in_characters[int(s.pop())*10])
+    if i==2:
+        length_in_letters += len(numbers_in_characters[int(x[0]+"0")])
+        i = -1
+        x = x[1]
 
-    if s != []:
-        in_letters += len(numbers_in_characters[int(s.pop())])
-        in_letters += len(numbers_in_characters[100])
-        in_letters += len("and")
-    if s != []:
-        in_letters += len(numbers_in_characters[1000])
+    if i==1:
+        length_in_letters += len(numbers_in_characters[int(x[0])])
 
-
-    return in_letters
+    return length_in_letters
 
 
 
 
-#main function:
+# Main function:
+
 
 length=0
 for i in range(1,1001):
