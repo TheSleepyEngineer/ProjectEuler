999fe1d307748cb599bd0cae36eb6ba50930f793
mahmoud <mahmoud.odb@gmail.com>
2019-03-10 09:38:03 -0400

    Resolved

---
+++ 18-Maximum_Path_sum_1.py
@@ -0,0 +1,29 @@
+#Fill Values into Matrix M
+
+M=[]
+
+file=open("/tmp/MaximumPathSum_1","r")
+lines=file.readlines()
+
+for line in lines:
+    table=[]
+    for integers in line.split():
+        table.append(int(integers))
+    M.append(table)
+
+number_of_rows= len(M)
+last_row=number_of_rows -1
+
+
+
+# Main program :
+
+
+for i in range(last_row-1, -1, -1):
+    number_of_columns= len(M[i])
+
+
+    for j in range(0,number_of_columns):
+        M[i][j]= M[i][j] + max(M[i+1][j], M[i+1][j+1])
+
+print(M[0][0])
